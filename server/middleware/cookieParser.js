9d2fbe9209043ba8f153b173f105a65823759062
Imran Kheraj <ikheraj17@gmail.com>
2020-04-17 23:00:57 -0500

    implemented cookie parser middleware function

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,22 @@
 const parseCookies = (req, res, next) => {
+
+  if(!req.headers.cookie){
+    req.cookies = {};
+    next();
+    return;
+  }
+
+  var cookieObj = {};
+
+  var cookies = req.headers.cookie.split('; ');
+  cookies.forEach((cookie) => {
+    var cookieArray = cookie.split('=');
+    cookieObj[cookieArray[0]] = cookieArray[1];
+  });
+
+  req.cookies = cookieObj;
+  next();
 };
+
 
 module.exports = parseCookies;
