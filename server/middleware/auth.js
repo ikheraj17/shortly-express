a95d0ee2b90b5ffb0435f6c51eed5dc8a901b7c5
Matt McGavern <matt.mcgavern@gmail.com>
2020-04-18 14:11:08 -0500

    Refactored initial conditional for createSession

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,24 +3,24 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
 
-  // console.log('this is our cookies obj', req.cookies);
-  if (Object.keys(req.cookies).length === 0) {
+  if (!req.cookies.shortlyid) {
     return models.Sessions.create()
       .then((data) => {
         var id = data.insertId;
         return models.Sessions.get({id});
       })
       .then((sessionHash) => {
-        console.log('should be a hash?!', sessionHash);
+        // console.log('should be a hash?!', sessionHash);
         req.session = { hash: sessionHash.hash };
         res.cookies = { shortlyid: sessionHash.hash };
-        console.log('this is the response', res.cookies);
-        console.log('this is the request', req.session);
+        // console.log('this is the response', res.cookies);
+        // console.log('this is the request', req.session);
         // return;
         next();
       });
   } else {
-
+    console.log('this is the else part: ', req);
+    // return models.Sessions.get()
   }
 
 };
